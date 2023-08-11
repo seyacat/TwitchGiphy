845515ee78c331824bffbb0ffc1eaa4fa2ad1bad
Seyacat <seyacat@gmail.com>
2023-08-10 22:48:46 -0500

    Ini Commit

---
+++ giphy.js
@@ -0,0 +1,63 @@
+import twitch from "./twitch.js";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const apiKey = urlParams.get("api-key") ?? localStorage.apikey;
+const customRewardId =
+  urlParams.get("custom-reward-id") ?? "37f43acd-8b34-42cf-8392-39f2ebda97ad";
+
+const constainer = document.getElementById("container");
+const image = document.getElementById("image");
+const msg = document.getElementById("msg");
+const msgQueue = [];
+let timeout = 0;
+
+const gifQueu = function (msgData) {
+  if (
+    msgData.cmd != "PRIVMSG" ||
+    msgData?.["custom-reward-id"] != customRewardId
+  ) {
+    return;
+  }
+  msgQueue.push(msgData);
+};
+
+const getRandomGif = async function (msgData) {
+  console.log({ msgData });
+  if (
+    msgData.cmd != "PRIVMSG" ||
+    msgData?.["custom-reward-id"] != customRewardId
+  ) {
+    return;
+  }
+  const ret = await (
+    await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${msgData.msg}`,
+      { method: "GET" }
+    )
+  ).json();
+  image.src = ret.data.images.original.url;
+  msg.innerText = `${msgData?.["display-name"]}:${msgData.msg}`;
+};
+
+twitch(gifQueu);
+
+image.addEventListener("load", () => {
+  timeout = 8;
+  constainer.classList.remove("on");
+  constainer.style.opacity = 1;
+});
+
+const tic = function () {
+  if (timeout <= 0) {
+    if (msgQueue.length) {
+      getRandomGif(msgQueue.shift());
+      timeout = 5;
+    } else {
+      constainer.classList.add("on");
+    }
+  }
+  timeout--;
+};
+
+setInterval(tic, 1000);
---
+++ twitch.js
@@ -0,0 +1,60 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const channel = urlParams.get("channel") ?? "seyacat";
+
+const subscribers = [];
+export default (f) => {
+  subscribers.push(f);
+};
+
+// Crear una instancia de WebSocket
+const ws = new WebSocket("wss://irc-ws.chat.twitch.tv:443");
+
+// Evento que se ejecuta cuando la conexión se abre
+ws.addEventListener("open", (event) => {
+  console.log("Conexión establecida");
+  ws.send("CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands");
+  ws.send("NICK justinfan12345");
+  ws.send("PASS kappa");
+  ws.send("JOIN #" + channel);
+  // Enviar un mensaje al servidor
+});
+
+// Evento que se ejecuta cuando se recibe un mensaje del servidor
+ws.addEventListener("message", (event) => {
+  let msg = event.data;
+  let data = {};
+  for (let pair of msg.split("!", 2)[0].split(";")) {
+    let p = pair.split("=");
+    if (p.length == 2) {
+      data[p[0]] = p[1];
+    }
+  }
+  let remsg =
+    /(?:(([a-zA-Z0-9_]*?)!([a-zA-Z0-9_]*?)@[a-zA-Z0-9_]*?.tmi.twitch.tv)|tmi.twitch.tv)\s([A-Z]*?)?\s#([^\s]*)\s{0,}:?(.*?)?$/gim.exec(
+      msg
+    ); // ['cd', undefined, 'cd']
+
+  if (remsg?.length >= 7) {
+    data["username"] = remsg[2];
+    data["cmd"] = remsg[4];
+    data["channel"] = remsg[5];
+    data["msg"] = remsg[6];
+  }
+
+  for (const f of subscribers) {
+    if (typeof f == "function") {
+      f(data);
+    }
+  }
+});
+
+// Evento que se ejecuta cuando la conexión se cierra
+ws.addEventListener("close", (event) => {
+  console.log("Conexión cerrada");
+});
+
+// Evento que se ejecuta en caso de error
+ws.addEventListener("error", (event) => {
+  console.error("Error en la conexión:", event);
+});
